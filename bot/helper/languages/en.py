f8c9b668ea16fce93007bc050931fd8b0f4cf745
Satyam <newharyanvi0077@gmail.com>
2026-09-09 19:14:51 +0530

    Update en.py

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -1,6 +1,9 @@
 START_MSG = """
-WELCOME TO @FlashMirror.
-How Aer You
+✺ Welcome to Poster Pro Bot ✺
+
+• This Bot Scrapes Posters from various OTT Platforms and Bypasses direct links from Cloud Websites.
+
+• Completely Free & Open Source.
 """
 START_BUTTON1 = "Repo"
 START_BUTTON2 = "Updates"
